b4300f5818ea9c778756d78e4aaebe58664e9831
iansharff <ips11@georgetown.edu>
2021-08-23 17:43:26 -0400

    Update plotting function and stylesheet

--- tools/visualizations.py
+++ tools/visualizations.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 def plot_loss_accuracy(results):
-    fig, axes = plt.subplots(1, 2, figsize=(10, 15))
+    fig, axes = plt.subplots(1, 2, figsize=(15, 8))
 
     acc = results.history['acc']
     loss = results.history['loss']
@@ -19,16 +19,18 @@ def plot_loss_accuracy(results):
 
     epochs = np.arange(1, len(acc) + 1)
 
-    axes[0].plot(epochs, acc)
-    axes[0].plot(epochs, val_acc)
+    axes[0].plot(epochs, acc, label="Training Accuracy")
+    axes[0].plot(epochs, val_acc, label="Validation Accuracy")
     axes[0].set_title("Train and Validation Accuracies")
     axes[0].set_xlabel("Epoch")
     axes[0].set_ylabel("Accuracy")
+    axes[0].legend()
 
-    axes[1].plot(epochs, loss)
-    axes[1].plot(epochs, val_loss)
+    axes[1].plot(epochs, loss, label='Training Loss')
+    axes[1].plot(epochs, val_loss, label='Validation Loss')
     axes[1].set_title("Train and Validation Loss")
     axes[1].set_xlabel("Epoch")
     axes[1].set_ylabel("Binary cross-entropy loss")
+    axes[1].legend()
 
     return fig
